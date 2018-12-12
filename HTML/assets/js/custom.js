66fc0d5ea03093cf610639862f51b9424cc0bf05
mykolas <mykolas666@gmail.com>
2018-12-12 13:48:15 +0200

    Add Project HTML part

---
+++ HTML/assets/js/custom.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:true
+});
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# Antraste
+# WEB programming full-time courses project
 
-## Mazesne antraste
+> This is my project. I have been working on it during the course.
+> First I created a static HTML page. 
+> Then was added some JavaScript to make the website more dynamic.
+> After all, the project was mounted on WordPress platform.
+
+- In HTML folder you will find static page.
